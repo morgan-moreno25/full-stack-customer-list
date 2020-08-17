ad9223bf2f45bc267d86466480f1b46831205bd5
Morgan <morgan.moreno25@gmail.com>
2020-08-17 12:37:10 -0500

    installation and clean up of react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div id="app">
+        
+      </div>
+    )
+  }
+};
+
+export default App;
