d3aa765dd9f7171af9a45f0dbfa30fe866dbe0ad
Morgan <morgan.moreno25@gmail.com>
2020-08-19 12:04:07 -0500

    v1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const DB = require('./config/DB');
 const requestHeaders = require('./middleware/headers');
 const cors = require('cors');
+const path = require('path');
 
 const baseRouter = require('./routes/api');
 
@@ -17,6 +18,16 @@ app.use(cors());
 
 // Routing
 app.use('/api', baseRouter);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
 
 // Connect app to server
 const PORT = process.env.PORT || 5000;
