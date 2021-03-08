93baa379494c3a8ea6afcb99f217ee90c080dff9
Morgan <morgan.moreno25@gmail.com>
2021-03-08 10:12:49 -0600

    refactored auth / customer in redux to take advantage of the simplicity of '@reduxjs/toolkit'

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,19 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
 
+import authReducer from './slices/auth.slice';
+import customerReducer from './slices/customer.slice';
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
-
-export default store;
+export default configureStore({
+    reducer: {
+        auth: authReducer,
+        customer: customerReducer,
+    }
+})
