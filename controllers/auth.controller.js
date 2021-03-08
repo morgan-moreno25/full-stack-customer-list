f9772d6dd093ff58839e96a0fb7ff4ca71fd7a77
Morgan <morgan.moreno25@gmail.com>
2021-03-08 08:52:16 -0600

    extracted auth route logic into a seperate controller file

---
+++ controllers/auth.controller.js
@@ -0,0 +1,60 @@
+const AuthController = (() => {
+    const bcrypt = require('bcryptjs');
+    const config = require('../utils/config');
+    const jwt = require('jsonwebtoken');
+    const User = require('../models/User');
+
+
+    const loadUser = async (req, res, next) => {
+        try {
+            const user = await User.findById(req.user.id);
+
+            return res.status(200).json(user);
+        }catch(error){
+            return res.status(400).json(error);
+        };
+    };
+    const login = async (req, res, next) => {
+        const { email, password } = req.body;
+        var error;
+        
+        if(!email || !password){
+            return res.status(400).json({ msg: "Please enter all fields" });
+        }
+
+        try {
+            const validUser = await User.findOne({ username });
+
+            if(validUser){
+                const isMatch = await bcrypt.compare(password, validUser.password);
+
+                if(isMatch){
+                    const token = await jwt.sign({ id: validUser._id }, config.SECRET, { expiresIn: 3600 });
+
+                    return res.status(200).json({
+                        token,
+                        user: {
+                            id: validUser._id,
+                            name: validUser.name,
+                            email: validUser.email,
+                        }
+                    });
+                }else{
+                    return res.status(400).json({ msg: 'Invalid credentials' });
+                }
+            }else{
+                return res.status(400).json({ msg: 'User does not exist' });
+            }
+
+        }catch(error){
+            return res.status(400).json(error);
+        };
+    };
+
+    return {
+        loadUser,
+        login,
+    }
+})();
+
+module.exports = AuthController;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
+const authController = require('../../controllers/auth.controller');
 
-const User = require('../../models/User');
 const router = express.Router();
 
 /**
@@ -12,45 +9,13 @@ const router = express.Router();
  * @info Authorize a user
  * @access Public
  */
-router.post('/', ( req, res ) => {
-    const { email, password } = req.body;
-
-    if(!email || !password){
-        return res.status(400).json({ msg: "Please enter all fields" });
-    };
-
-    User.findOne({ email: email })
-        .then(user => {
-            if(!user) return res.status(400).json({ msg: "User does not exist" });
-
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if(!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
-
-                    jwt.sign({ id: user.id }, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
-                        if(err) throw err;
-                        res.json({
-                            token: token,
-                            user: {
-                                id: user.id,
-                                name: user.name,
-                                email: user.email,
-                            }
-                        });
-                    });
-                });
-        });
-});
+router.post('/', authController.login);
 
 /**
  * @route GET api/auth/user
  * @info Get user data
  * @access Private
  */
-router.get('/user', auth, ( req, res ) => {
-    User.findById(req.user.id)
-        .select('-password')
-        .then(user => res.json(user))
-});
+router.get('/user', auth, authController.loadUser);
 
 module.exports = router;
