a4feeb6b0de1b83a00100dcd170efb31e31ab30a
Morgan <morgan.moreno25@gmail.com>
2021-03-08 08:56:48 -0600

    updated status codes for server errors

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,7 +11,7 @@ const AuthController = (() => {
 
             return res.status(200).json(user);
         }catch(error){
-            return res.status(400).json(error);
+            return res.status(500).json(error);
         };
     };
     const login = async (req, res, next) => {
@@ -47,7 +47,7 @@ const AuthController = (() => {
             }
 
         }catch(error){
-            return res.status(400).json(error);
+            return res.status(500).json(error);
         };
     };
 
