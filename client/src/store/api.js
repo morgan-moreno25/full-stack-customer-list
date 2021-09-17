b43686080161324c6f49e178661b7c692ce237d9
Morgan <morgan.moreno25@gmail.com>
2021-09-17 08:28:13 -0500

    Refactor: Added Production URL for Api Calls

--- client/src/store/api.js
+++ client/src/store/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const DEV_URL = 'http://localhost:5000/api/v1';
+const PROD_URL = 'https://mern-customer-list.herokuapp.com/api/v1';
+
 export const api = axios.create({
-	baseURL: 'http://localhost:5000/api/v1',
+	baseURL: process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL,
 	headers: {
 		'Content-Type': 'application/json',
 	},
